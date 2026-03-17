7beeb1fd6d417401069c0ddf1410c1b856200284
Amaresh Chandra <67590671+Sarojamar@users.noreply.github.com>
2026-03-17 13:11:32 +0530

    Clean up Sidebar
    Removed unused links and playlist section from the sidebar.

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,15 +6,10 @@
       <div className="sidebar">
         <div className="sidebar-menu">
           <a href="#">Home</a>
-          <a href="#">Search</a>
-          <a href="#">Your Library</a>
+          
         </div>
         <div className="sidebar-playlists">
-          <h4>Your Playlists</h4>
-          <ul>
-            <li>Playlist 1</li>
-            <li>Playlist 2</li>
-            <li>Playlist 3</li>
+          
           </ul>
         </div>
       </div>
